5ce121aa56ef3f1e2b1656258200330f3df324c1
jemacarse <jemacarse@gmail.com>
2016-08-10 16:41:32 +0200

    Version du 10/08

---
+++ src/java/jemacarse/servlet/GenererTable.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jemacarse.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "GenererTable", urlPatterns = {"/GenererTable"})
+public class GenererTable extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Persistence.createEntityManagerFactory("JemacarsePU");}
+
+  
+    
+
+}
